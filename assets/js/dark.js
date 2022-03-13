af79566ecbe498b3fee58ebd338d8eb6eff59883
DmytroZavhorodniy <93467318+DmytroZavhorodniy@users.noreply.github.com>
2022-03-13 21:13:33 +0200

    Add files via upload

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,11 +1,11 @@
 function dark(){
     document.getElementById("darklink").href=  "./assets/darkstyle.css";
-    theme.innerHTML = '<button onclick="light()" type="button" class="btn btn-outline-light">Світла тема</button>';
+    theme.innerHTML = '<button onclick="light()" type="button" class="btn btn-outline-light">Jasny temat</button>';
     document.getElementById("webinterestlink").href=  "webinterestdark.html";
     
 }
 function light(){
     document.getElementById("darklink").href=  " ";
-    theme.innerHTML = ' <button onclick="dark()" type="button" class="btn btn-outline-dark">Темна тема</button>';
+    theme.innerHTML = ' <button onclick="dark()" type="button" class="btn btn-outline-dark">Ciemny schemat</button>';
     document.getElementById("webinterestlink").href=  "webinterest.html";
 }
